38527850f5c58e782b650c48f568520a43ca5914
tapandscore <rupamsarmah.duiet@gmail.com>
2026-08-22 21:07:37 +0530

    Add files via upload

---
+++ app/forgot-password/page.js
@@ -0,0 +1,11 @@
+export const dynamic = "force-dynamic";
+
+import ForgotPasswordForm from "./ForgotPasswordForm";
+
+export const metadata = {
+  title: "Reset your password",
+};
+
+export default function ForgotPasswordPage() {
+  return <ForgotPasswordForm />;
+}
